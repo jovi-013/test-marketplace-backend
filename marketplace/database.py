3ede81020d46be8beff53485f641ca802d100441
Jovi H <jovi.hutama@garasi.id>
2025-08-06 14:59:26 +0700

    Fix db for deployment

--- marketplace/database.py
+++ marketplace/database.py
@@ -13,10 +13,12 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-if settings.DATABASE_URL.startswith("postgresql://"):
-    settings.DATABASE_URL = settings.DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+if database_url.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+elif database_url.startswith("postgresql://"):
+    database_url = database_url.replace("postgresql://", "postgresql+asyncpg://", 1)
 
-engine = create_async_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_async_engine(settings.DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
